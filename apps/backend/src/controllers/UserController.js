f63658067c91348a223670d9f3ce75fb4f21ae30
Guilherme Holanda <guilhermeholanda519@gmail.com>
2025-10-10 11:57:42 -0300

    ft: adjust lint to create a pull request to review

--- apps/backend/src/controllers/UserController.js
+++ apps/backend/src/controllers/UserController.js
@@ -1,40 +1,40 @@
 import UserService from "../services/UserService.js";
 
 class UserController {
-  async store(req, res) {
-    try {
-      const user = await UserService.createUser(req.body);
-      const { id, name, email } = user;
-      return res.json({ id, name, email });
-    } catch (error) {
-      switch (error.message) {
-        case "Email already registered.":
-          return res.status(409).json({ error: "A user with this email already exists." });
-        default:
-          return res.status(500).json({ error: "Failed to create user." });
-      }
-    }
-  }
+	async store(req, res) {
+		try {
+			const user = await UserService.createUser(req.body);
+			const { id, name, email } = user;
+			return res.json({ id, name, email });
+		} catch (error) {
+			switch (error.message) {
+				case "Email already registered.":
+					return res.status(409).json({ error: "A user with this email already exists." });
+				default:
+					return res.status(500).json({ error: "Failed to create user." });
+			}
+		}
+	}
 
-  async update(req, res) {  
-    try {
-      const user = await UserService.updateUser(req.userId, req.body);
-      const { id, name, email } = user;
-      return res.json({ id, name, email });
-    } catch (error) {
-      console.error(error);
-      switch (error.message) {
-        case "User not found.":
-          return res.status(404).json({ error: "User not found." });
-        case "Email already registered.":
-          return res.status(409).json({ error: "A user with this email already exists." });
-        case "Incorrect password.":
-          return res.status(403).json({ error: "Incorrect password." });
-        default:
-          return res.status(500).json({ error: "Failed to update user." });
-      }
-    }
-  }
+	async update(req, res) {
+		try {
+			const user = await UserService.updateUser(req.userId, req.body);
+			const { id, name, email } = user;
+			return res.json({ id, name, email });
+		} catch (error) {
+			console.error(error);
+			switch (error.message) {
+				case "User not found.":
+					return res.status(404).json({ error: "User not found." });
+				case "Email already registered.":
+					return res.status(409).json({ error: "A user with this email already exists." });
+				case "Incorrect password.":
+					return res.status(403).json({ error: "Incorrect password." });
+				default:
+					return res.status(500).json({ error: "Failed to update user." });
+			}
+		}
+	}
 }
 
 export default new UserController();
--- apps/backend/src/services/LikeService.js
+++ apps/backend/src/services/LikeService.js
@@ -3,36 +3,36 @@ import PostLike from "../models/PostLike.js";
 import { delCache } from "../config/redis.js";
 
 class LikeService {
-  async toggleLike({ post_id, user_id }) {
-    const like = await sequelize.transaction(async (t) => {
-      let existingLike = await PostLike.findOne({
-        where: { 
-          post_id, 
-          user_id 
-        },
-        transaction: t,
-      });
+	async toggleLike({ post_id, user_id }) {
+		const like = await sequelize.transaction(async (t) => {
+			let existingLike = await PostLike.findOne({
+				where: {
+					post_id,
+					user_id
+				},
+				transaction: t,
+			});
 
-      if (!existingLike) {
-        existingLike = await PostLike.create(
-          { 
-            post_id, 
-            user_id, 
-            is_deleted: false 
-          },
-          { transaction: t }
-        );
-      } else {
-        existingLike.is_deleted = !existingLike.is_deleted;
-        await existingLike.save({ transaction: t });
-      }
+			if (!existingLike) {
+				existingLike = await PostLike.create(
+					{
+						post_id,
+						user_id,
+						is_deleted: false
+					},
+					{ transaction: t }
+				);
+			} else {
+				existingLike.is_deleted = !existingLike.is_deleted;
+				await existingLike.save({ transaction: t });
+			}
 
-      return existingLike;
-    });
+			return existingLike;
+		});
 
-    await delCache("posts:*");
-    return { liked: !like.is_deleted };
-  }
+		await delCache("posts:*");
+		return { liked: !like.is_deleted };
+	}
 }
 
 export default new LikeService();
